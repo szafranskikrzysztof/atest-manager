9588f8907257ec5b14dc5ed4a20f763d962ee7f0
Krzysztof <krz.szafranski@gmail.com>
2019-08-20 18:03:11 +0200

    Initial commit

---
+++ src/main/java/com/atestmanager/AtestManagerApplication.java
@@ -0,0 +1,13 @@
+package com.atestmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtestManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtestManagerApplication.class, args);
+    }
+
+}
